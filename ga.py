f1cf902b84b98558777efc0bdd0640a06148216d
Stuart Stobie <stuartstobie@gmail.com>
2016-02-20 22:47:19 +0000

    Commented and adjusted

--- ga.py
+++ ga.py
@@ -1,6 +1,7 @@
 # TODO May only need sqrt from math and uniform from random
 from math import sqrt
 from random import choice, randint, sample, uniform
+from time import time
 
 # POP_LIMIT / TOURNAMENT_LIMIT must be an even number for crossover!
 POP_LIMIT = 200
@@ -10,6 +11,19 @@ INITIAL_MUTATION_FACTOR = 0.1
 SECONDARY_MUTATION_CHANCE = 10
 SECONDARY_MUTATION_FACTOR = 0.8
 CRAZY_MUTATION_CHANCE = 5
+
+#  Create report file
+timestamp = time()
+f = open(str(timestamp) + '.csv','w')
+f.write('POP_LIMIT: ' + str(POP_LIMIT) + '\n')
+f.write('TOURNAMENT_LIMIT: ' + str(TOURNAMENT_LIMIT) + '\n')
+f.write('BOUNDS: ' + str(BOUNDS) + '\n')
+f.write('INITIAL_MUTATION_FACTOR: ' + str(INITIAL_MUTATION_FACTOR) + '\n')
+f.write('SECONDARY_MUTATION_CHANCE: ' + str(SECONDARY_MUTATION_CHANCE) + '\n')
+f.write('SECONDARY_MUTATION_FACTOR: ' + str(SECONDARY_MUTATION_FACTOR) + '\n')
+f.write('CRAZY_MUTATION_CHANCE: ' + str(CRAZY_MUTATION_CHANCE) + '\n\n')
+f.write('Generation, Best Fitness, Worst Fitness, Average Fitness, Best Chromosomes\n')
+f.close()
 
 # Open data file
 data_file = open('datfile.dat', 'r')
@@ -21,6 +35,7 @@ data_file.close()
 X_DATA = data[::2] # even values are x coordinates
 Y_DATA = data[1::2] # odd values are y coordinates
 
+# Returns f(x) values for a given candidate. Used in fitness calculation.
 def calculate_y_values(candidate):
     y_values = []
     for index, x in enumerate(X_DATA):
@@ -30,6 +45,8 @@ def calculate_y_values(candidate):
         y_values.append(y)
     return y_values
 
+# Returns the fitness for a given candidate, higher is better.
+# Compares calculated f(x) values to the actual ones.
 def calculate_fitness(candidate):
     fitness = 0
     cand_y_values = calculate_y_values(candidate)
@@ -39,6 +56,7 @@ def calculate_fitness(candidate):
     fitness = 1 / fitness
     return fitness
 
+# Returns a single candidate.
 def generate_candidate():
     candidate = {'chromosomes': [], 'fitness': 0}
     for i in range(6):
@@ -46,17 +64,20 @@ def generate_candidate():
     candidate['fitness'] = calculate_fitness(candidate)
     return candidate
 
+# Returns a single candidate, created from given chromosomes.
 def create_candidate(chromosomes):
     candidate = {'chromosomes': chromosomes, 'fitness': 0}
     candidate['fitness'] = calculate_fitness(candidate)
     return candidate
 
+# Returns an entire population, used at the start.
 def generate_population():
     population = []
     for i in range(POP_LIMIT):
         population.append(generate_candidate())
     return population
 
+# Randomly matches up a tournament and selects the winner. Returns a reduced population.
 def tournament_selection(population):
     new_population = []
     while(len(population) > 0):
@@ -69,6 +90,8 @@ def tournament_selection(population):
         new_population.append(winner)
     return new_population
 
+# Combines the chromosomes of two parents. Returns a new population with parents and offspring.
+# This doubles the size of the given population.
 def crossover(population):
     new_population = []
     next_gen = []
@@ -82,11 +105,14 @@ def crossover(population):
     new_population = new_population + next_gen
     return new_population
 
+# Randomely return 1 or -1. Used to determine whether mutation increases or decreases values.
 def shrink_or_grow():
     if randint(0,1) == 0:
         return -1
     return 1
 
+# Mutates at least one chromosome in each candidate. Returns a population of original and
+# mutated candidates. Doubles the size of the given population.
 def mutation(population):
     mutants = []
     for candidate in population:
@@ -105,13 +131,15 @@ def mutation(population):
     population = population + mutants
     return population
 
+# brings the population back up to the limit. This is only used if tournament size > 4.
 def top_up(population):
     # if len(population) < POP_LIMIT:
     #     print('Topping up: ', POP_LIMIT - len(population))
     while(len(population) < POP_LIMIT):
         population.append(generate_candidate())
 
-def reporter(population):
+# Writes report on the given population.
+def reporter(population, gen):
     best = population[1]
     worst = best
     total_fitness = 0
@@ -122,10 +150,19 @@ def reporter(population):
             worst = candidate
         total_fitness += candidate['fitness']
     average = total_fitness / len(population)
+    f = open(str(timestamp) + '.csv','a')
+    f.write(str(i) + ', ')
+    f.write(str(best['fitness']) + ', ')
+    f.write(str(worst['fitness']) + ', ')
+    f.write(str(average) + ', ')
+    f.write(str(best['chromosomes']) + '\n')
+    f.close()
+    print('Generation: ', i)
     print('Best: ', best['fitness'])
-    print(best['chromosomes'])
     print('Worst:', worst['fitness'])
     print('Average: ', average)
+    print(best['chromosomes'])
+
 
 population = generate_population()
 
@@ -134,13 +171,7 @@ for i in range(0,10000):
     population = crossover(population)
     population = mutation(population)
     top_up(population)
-    print('Generation: ', i, 'Population: ', len(population))
-    reporter(population)
-
-# population = generate_population()
-# print(len(population))
-# population = tournament_selection(population)
-# print(len(population))
+    reporter(population, i)
 
 # custom_gen = [-0.00318,5000,5,-62,1,-0.001]
 # print(custom_gen, calculate_fitness(custom_gen))
